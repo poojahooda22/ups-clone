f58e3c41c1c66b670989f6c4594effe068afc804
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-14 12:23:15 +0530

    create order screens

---
+++ screens/OrderScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OrderScreen = () => {
+  return (
+    <View>
+      <Text>This is order screen!</Text>
+    </View>
+  )
+}
+
+export default OrderScreen
+
+const styles = StyleSheet.create({})
