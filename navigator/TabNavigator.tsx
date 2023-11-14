d48a33638dfca7600267332c36abddd6735f91d3
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-14 13:05:05 +0530

    updated navigator

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomerScreen from '../screens/CustomerScreen';
 import OrderScreen from '../screens/OrderScreen';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from "@rneui/themed"
 
 
 export type TabStackParamList = {
@@ -20,10 +21,34 @@ const TabNavigator = () => {
         navigation.setOptions({
             headerShown: false
         });
-    }, [navigation]);
+    }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+        screenOptions={({route}) => ({
+            tabBarActiveTintColor: "#59C1CC",
+            tabBarInactiveTintColor: "gray",
+            tabBarIcon: ({focused, color, size}) => {
+                if(route.name === 'Customers') {
+                    return (
+                        <Icon
+                            name='users'
+                            type='entypo'
+                            color={focused ? '#59c1cc' : 'gray'}
+                        />
+                    );
+                } else if (route.name === 'Orders') {
+                    return (
+                        <Icon
+                            name='box'
+                            type='entypo'
+                            color={focused ? '#eb6a7c' : 'gray'}
+                        />
+                    );
+                }
+            }
+        })}
+    >
       <Tab.Screen name="Customers" component={CustomerScreen} />
       <Tab.Screen name="Orders" component={OrderScreen} />
     </Tab.Navigator>
