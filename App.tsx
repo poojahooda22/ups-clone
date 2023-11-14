f8eb0e5b0f4ea21ab41c50050de3669bf5d9bafc
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-14 12:22:27 +0530

    update app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet} from 'react-native';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import CustomerScreen from './screens/CustomerScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './navigator/RootNavigator';
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
+    
     <TailwindProvider utilities={utilities}>
-      <CustomerScreen />
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
    </TailwindProvider>
     
   );
