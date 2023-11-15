fc0a529132358fe411b11a4c091c64f706e3780e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-15 16:30:55 +0530

    updated customer screens

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -8,7 +8,9 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { Image } from "@rneui/themed"
 import { Input } from '@rneui/base';
-
+import { GET_CUSTOMERS } from '../graphql/queries';
+import { useQuery } from '@apollo/client';
+import CustomerCard from '../components/CustomerCard';
 
 export type CustomerScreenNavigationProp = CompositeNavigationProp<
     BottomTabNavigationProp<TabStackParamList, "Customers">,
@@ -19,6 +21,7 @@ const CustomerScreen = () => {
     const tw = useTailwind();
     const navigation = useNavigation<CustomerScreenNavigationProp>();
     const [input, setInput] = useState<string>("");
+    const { loading, error, data } = useQuery(GET_CUSTOMERS);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -27,7 +30,7 @@ const CustomerScreen = () => {
     }, []);
 
   return (
-    <ScrollView>
+    <ScrollView style={{backgroundColor: "#59C1CC"}} >
       <Image source={{uri: "https://links.papareact.com/3jc"}} 
         containerStyle={tw("w-full h-64")}
         PlaceholderContent={<ActivityIndicator />}
@@ -40,9 +43,18 @@ const CustomerScreen = () => {
         containerStyle={tw("bg-white pt-5 pb-0 px-10")}
       />
 
+      {data?.getCustomers
+        ?.filter((customer: CustomerList) => 
+        customer.value.name.includes(input)
+        )
+        .map(
+        ({ name: ID, value: {email, name}}: CustomerResponse) => (
+          <CustomerCard key={ID} name={name} email={email} userId={ID} />
+        )
+      )}
     </ScrollView>
-  )
-}
+  );
+};
 
 export default CustomerScreen
 
