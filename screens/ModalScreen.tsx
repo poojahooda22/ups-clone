92af69a69c2eda31a15640cad6e12dbe3b615d87
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-15 18:34:26 +0530

    updated modal screen

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,5 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { Icon } from '@rneui/themed';
+import { useNavigation, CompositeNavigationProp } from '@react-navigation/native';
+// import { useTailwind } from 'tailwind-rn/dist';
+// import { RootStackParamList } from '../navigator/RootNavigator';
+// import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+// import { TabStackParamList } from '../navigator/TabNavigator';
+// import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+
+
 
 const ModalScreen = () => {
   return (
