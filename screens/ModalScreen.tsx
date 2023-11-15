e913dc0d7733b92ffcf9fb6c7c11347dbe035385
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-15 18:35:57 +0530

    updated

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@rneui/themed';
 import { useNavigation, CompositeNavigationProp } from '@react-navigation/native';
 import { useTailwind } from 'tailwind-rn/dist';
 import { RootStackParamList } from '../navigator/RootNavigator';
-// import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 // import { TabStackParamList } from '../navigator/TabNavigator';
 // import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
