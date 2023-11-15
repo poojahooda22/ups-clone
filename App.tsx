a74b7c959f50a9cde6fbf9394af7dfe496a06246
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-15 16:29:18 +0530

    updated app

--- App.tsx
+++ App.tsx
@@ -5,25 +5,27 @@ import utilities from './tailwind.json';
 import CustomerScreen from './screens/CustomerScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'https://capelleaandenijssel.stepzen.net/api/illmannered-anteater',
+  cache: new InMemoryCache(),
+});
+
+
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
    </TailwindProvider>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
