243512c69d5f8e02f7ec61651f39940e15c1b62d
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-16 12:24:07 +0530

    updated

--- screens/OrderScreen.tsx
+++ screens/OrderScreen.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, Text, View } from 'react-native'
+import React, {useState, useLayoutEffect} from 'react';
+import { CompositeNavigationProp,  useNavigation } from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigator/RootNavigator';
+import { useTailwind } from 'tailwind-rn/dist';
+import useOrders from '../hooks/useOrders';
+import { Image } from "@rneui/themed"
+
+export type OrderScreenNavigationProp = CompositeNavigationProp<
+    BottomTabNavigationProp<TabStackParamList, "Orders">,
+    NativeStackNavigationProp<RootStackParamList>
+>;
 
 const OrderScreen = () => {
+  const tw = useTailwind();
+  const navigation = useNavigation<OrderScreenNavigationProp>();
+  const { loading, error, orders } = useOrders();
+  const [ascending, setAscending] = useState<boolean>(true);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({focused, color}) => (
+        <Text style={{color: focused ? "#EB6A7C" : color, fontSize: 10 }}>
+          Orders
+        </Text>
+      )
+    });
+  })
+
   return (
-    <View>
-      <Text>This is order screen!</Text>
-    </View>
+    <ScrollView>
+      <Image />
+    </ScrollView>
   )
 }
 
 export default OrderScreen
 
-const styles = StyleSheet.create({})
