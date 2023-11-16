c0d757ae14254a938a0ffb790689eede1220c3aa
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-16 12:23:13 +0530

    updated

--- components/CustomerCard.tsx
+++ components/CustomerCard.tsx
@@ -18,7 +18,14 @@ const CustomerCard = ({email, name, userId}: Props) => {
     const navigation = useNavigation<CustomerScreenNavigationProp>();
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity 
+            onPress={() => 
+                navigation.navigate("MyModal", {
+                    name: name,
+                    userId: userId,
+                })
+            }
+        >
             <Card containerStyle={tw("p-5 rounded-lg")}>
                 <View style={tw("flex-row justify-between")}>
                     <View>
