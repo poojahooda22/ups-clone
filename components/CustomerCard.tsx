84a6ee367eec341265f7920064284bb1556ee076
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-16 21:53:01 +0530

    updated

--- components/CustomerCard.tsx
+++ components/CustomerCard.tsx
@@ -14,7 +14,7 @@ type Props = {
 
 const CustomerCard = ({email, name, userId}: Props) => {
     const {loading, error, orders } = useCustomerOrders(userId); 
-    const tw = useTailwind();
+    const tailwind = useTailwind();
     const navigation = useNavigation<CustomerScreenNavigationProp>();
 
     return (
@@ -26,23 +26,26 @@ const CustomerCard = ({email, name, userId}: Props) => {
                 })
             }
         >
-            <Card containerStyle={tw("p-5 rounded-lg")}>
-                <View style={tw("flex-row justify-between")}>
+            <Card containerStyle={tailwind("p-5 rounded-lg")}>
+                <View>
+                <View style={tailwind("flex-row justify-between")}>
                     <View>
-                        <Text style={tw("text-2xl font-bold")}>{name}</Text>
-                        <Text style={tw("text-sm text-gray-500")}>ID: {userId}</Text>
+                        <Text style={tailwind("text-2xl font-bold")}>{name}</Text>
+                        <Text style={[tailwind("text-sm text-gray-500"), { color: "#59C1CC"}]}>ID: {userId}</Text>
                     </View>
 
-                    <View style={tw("flex-row items-center justify-end")}>
-                        <Text>{loading ? "loading..." : `${orders.length} x`}</Text>
+                    <View style={tailwind(" flex flex-row items-center justify-end")}>
+                        <Text style={{ color: "#59C1CC"}}>
+                            {loading ? "loading..." : `${orders.length} x`}</Text>
                         <Icon
-                            style={tw("mb-5 ml-auto")}
+                            style={tailwind("mb-5 ml-auto")}
                             name="box"
                             type="entypo"
                             color="#59C1CC"
                             size={50}
                         />
                     </View>
+                </View>
                 </View>
                 <Card.Divider />
                 <Text>{email}</Text>
