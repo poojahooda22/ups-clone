19d0525c6987700090a1b09c524b4afe51dd3fd8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-17 12:47:52 +0530

    updated rootnavigator

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
 import ModalScreen from '../screens/ModalScreen';
+import OrderScreen from '../screens/OrderScreen';
 
 
 export type RootStackParamList = {
@@ -11,7 +12,7 @@ export type RootStackParamList = {
         userId: string;
         name: string;
     }
-    Order: { order: any}
+    Order: { order: Order}
 }
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
@@ -33,6 +34,10 @@ const RootNavigator = () => {
           name="MyModal" 
           component={ModalScreen} 
         />
+      </RootStack.Group>
+
+      <RootStack.Group>
+        <RootStack.Screen name="Order" component={OrderScreen} />
       </RootStack.Group>
     </RootStack.Navigator>
   )
