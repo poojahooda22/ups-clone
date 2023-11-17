0bb6591f95e4f3e5925f003468ba6c2af75c9d39
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-17 23:40:45 +0530

    up

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -33,7 +33,8 @@ const CustomerScreen = () => {
     <ScrollView 
       style={{backgroundColor: "#59C1CC"}} 
     >
-      <Image source={{uri: "https://links.papareact.com/3jc"}} 
+      <Image 
+      source={{uri: "https://links.papareact.com/3jc"}} 
         containerStyle={tw("w-full h-64")}
         PlaceholderContent={<ActivityIndicator />}
       />
